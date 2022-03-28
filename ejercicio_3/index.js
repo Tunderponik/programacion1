57712987b223fb0fe4510e9d1c7a81e8cae2e0f5
Tunderponik <gonzaloherreracipo@gmail.com>
2022-03-28 17:08:59 -0300

    28/3

---
+++ ejercicio_3/index.js
@@ -0,0 +1,14 @@
+function calcular_potencia() {
+    const tension = document.getElementById("inp_tension").value
+    const corriente = document.getElementById("inp_corriente").value
+    const potencia = tension * corriente
+
+    return potencia
+}
+
+function mostrar_respuesta()
+{
+
+    const respuesta=calcular_potencia()
+    alert(respuesta)
+}
--- variables_funciones/index.js
+++ variables_funciones/index.js
@@ -1,5 +1,22 @@
 //función clásica
 function mostrar(){
-    alert('Probando')
+    //forma 1
+    //const nombres = document.querySelector("#inp_nombres").value
+    //forma 2
+    const nombres = document.querySelector("#inp_nombres").value
+
+    //sin complete string
+    const mensaje = 'Nombre y apellido: ${nombres} - DNI'
+
+    alert(mensaje)
 }
 
+
+function mifuncion_con_retorno() {
+    const ejemplo = 'Probando funcio con retorno'
+    return ejemplo
+}
+
+function monstrar_respuesta() {
+    const respuesta = mifuncion_con_retorno()
+}
