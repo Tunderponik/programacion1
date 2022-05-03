a5b6f6bc5a3dbf01cd87778010989ff5507bbd2a
Tunderponik <gonzaloherreracipo@gmail.com>
2022-05-03 16:58:43 -0300

    Listo

--- tpevaluativo/tp1/main.js
+++ tpevaluativo/tp1/main.js
@@ -1,15 +1,21 @@
-function calcular_precio(){
-    const auto = document.getElementById("inp_auto").value
-}
+function descuento() {
 
-if (auto=="Ford Ka"){
+    const precio = document.getElementById("ford").value
+    var resultado = null
 
-}
-
-if (auto=="Ford Fiesta"){
-
-}
-
-if (auto=="Ford Focus"){
-
+    if (precio == 1950000) {
+       resultado =   1950000 - ((precio / 100) * 5) 
+    }
+    else {
+        if (precio === 2500000) {
+           resultado = 2560000 - ((precio / 100) * 10) 
+        }
+        else {
+            if (precio == 1750000) {
+                resultado =  1750000 - ((precio / 100) * 15) 
+            }
+        }
+    } 
+    
+    document.getElementById("mssg_resultado").textContent = 'Precio Final: ' + resultado
 }
