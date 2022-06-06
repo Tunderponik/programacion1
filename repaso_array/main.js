bc892c47d201fb4eb5d3901982b5de1c2e5f63af
Tunderponik <gonzaloherreracipo@gmail.com>
2022-06-06 17:05:33 -0300

    Array

---
+++ repaso_array/main.js
@@ -0,0 +1,13 @@
+let nintendo64 = ["Juego 1", "Juego 2"]
+
+var GameBoyAdvance = [{id:1, name:'Consola 1'},{id:2, name:'Consola 2'},{id:3, name:'Consola 3'}];
+GameBoyAdvance.splice(0,1)
+console.log(GameBoyAdvance)
+
+var PokemonMini = [{id:1, name:'Consola 1'},{id:2, name:'Consola 2'},{id:3, name:'Consola 3'}];
+PokemonMini.splice(0,1)
+console.log(PokemonMini)
+
+//
+console.log(nintendo64)
+
